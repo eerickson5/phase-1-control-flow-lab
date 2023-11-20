04da450b20a4f455b9583c1cedd861d27f0ba8cb
Elise Erickson <elisece717@gmail.com>
2023-11-20 13:36:01 -0500

    satisfies tests

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(val){
+  if(val <= 400){
+    return 'This one is on me!'
+  } else if (val < 2000){
+    return "That will be twenty bucks."
+  } else if (val <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(descriptor){
+  switch (descriptor) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you."
+    default: 
+      return 'Bye.'
+  }
 }
